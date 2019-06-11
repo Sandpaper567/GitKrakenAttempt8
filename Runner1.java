f6562c74f4c90e604cbb7847f4cfdb9d76c59399
Sahaj Patel <patelsahajemail@gmail.com>
2019-06-11 11:09:50 -0400

    Runner and Interface for printing quotient functionality created

--- Runner1.java
+++ Runner1.java
@@ -1,2 +1,10 @@
-public class Runner{
+public class Runner1{
+    interface printQuotient{
+        void printQuotientOfNums(float n1 ,float n2);
+    }
+
+    public static void main(String[] args){
+        printQuotient divide = (float num1, float num2) -> System.out.printf("Quotient is: %.3f\n", num1/num2);
+        divide.printQuotientOfNums(3.0f, 2.0f);
+    }
 }
