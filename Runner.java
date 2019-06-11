1b4246fac13300f4ea6f30ea5b0b988126ea5823
Lehar <mlehar2@gmail.com>
2019-06-11 11:09:55 -0400

    Runner and Interface for perfect square functionality created

--- Runner.java
+++ Runner.java
@@ -1,2 +1,12 @@
+import java.lang.Math;
+interface Square{
+    boolean isSquare(int x);
+ }
 public class Runner{
+    public static void main(String[] args){
+        Square theSquare = (int x) -> {
+									return (Math.pow((int)Math.sqrt(x),2) == x);
+								};
+        System.out.println(theSquare.isSquare(9));
+    }
 }
